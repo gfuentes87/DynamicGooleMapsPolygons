330d088a99be9b8335a0cf7d57c05c84617de6da
gfuentes87 <gustavo.fuentes87@gmail.com>
2016-02-26 23:24:42 -0300

    Added files via upload
    path on drag and edit line error Fixed

--- multiplePolygon.js
+++ multiplePolygon.js
@@ -27,30 +27,41 @@ function addLine(event){
 		    strokeOpacity: 0.8,
 		    strokeWeight: 2,
 		    editable:true,
+		    draggable: true,
 		    map:map
 	  	});
 
 		line.addListener('click',function(event){
 			var polygon = PolygonObj[actualId];
-			if(polygon.path.length > 3){
-				var path = polygon.path;
-				path.push(event.latLng);
-				var dynamicPolygon = new google.maps.Polygon({
-				    paths: path,
-				    strokeColor: strokeColor,
-				    strokeOpacity: 0.8,
-				    strokeWeight: 2,
-				    fillColor: fillColor,
-				    fillOpacity: 0.35,
-				    map:map,
-				    editable:true,
-				    draggable: true,
-			  	});
-			  	PolygonObj[actualId].path = path;
-			  	PolygonObj[actualId].polygon = dynamicPolygon;
-			  	PolygonObj[actualId].line.setMap(null);
-			  	PolygonObj[actualId].line = null;
-			  	actualId = false;
+			
+			var thisLat = event.latLng.lat();
+			var	thisLng = event.latLng.lng();
+			var polygonLat = polygon.path[0].lat();
+			var polygonLng = polygon.path[0].lng();
+
+			var equals = (thisLat == polygonLat && thisLng == polygonLng);
+
+			if(polygon.path.length > 3 ){
+				if(equals){
+					var path = polygon.path;
+					path.push(event.latLng);
+					var dynamicPolygon = new google.maps.Polygon({
+					    paths: path,
+					    strokeColor: strokeColor,
+					    strokeOpacity: 0.8,
+					    strokeWeight: 2,
+					    fillColor: fillColor,
+					    fillOpacity: 0.35,
+					    map:map,
+					    editable:true,
+					    draggable: true,
+				  	});
+				  	PolygonObj[actualId].path = path;
+				  	PolygonObj[actualId].polygon = dynamicPolygon;
+				  	PolygonObj[actualId].line.setMap(null);
+				  	PolygonObj[actualId].line = null;
+				  	actualId = false;
+			  	}
 			}
 		});
 
@@ -60,11 +71,12 @@ function addLine(event){
 	  	};
 		actualId = cod;
 	}else{
+
 		var polygon = PolygonObj[actualId];
-		var path  = polygon.path;
+		var path  = polygon.line.getPath();
 		var line = polygon.line;
 		path.push(latLng);
+		polygon.path = path.getArray();
 		line.setPath(path);
 	}
 }
-
